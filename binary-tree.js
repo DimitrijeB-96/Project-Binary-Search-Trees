f626e99ca9c9de20e03abf5e7cd67592665d3f7c
DimitrijeB-96 <mitabackalic96@gmail.com>
2023-09-19 11:39:47 +0200

    First 6 tasks completed

--- binary-tree.js
+++ binary-tree.js
@@ -3,6 +3,7 @@ import Node from './node.js';
 export default class Tree {
   constructor(array) {
     this.root = this.buildTree(array);
+    this.levelOrderTraversal = [];
   }
 
   _sortArray(array) {
@@ -36,6 +37,117 @@ export default class Tree {
     node.right = this.binaryTreeSort(array, mid + 1, end);
 
     return node;
+  }
+
+  insert(data, node = this.root) {
+    if (node === null) {
+      node = new Node(data);
+      return node;
+    }
+
+    if (data < node.data) {
+      node.left = this.insert(data, node.left);
+    } else if (data > node.data) {
+      node.right = this.insert(data, node.right);
+    }
+
+    return node;
+  }
+
+  delete(data, node = this.root) {
+    if (node === null) {
+      return node;
+    }
+
+    if (data < node.data) {
+      node.left = this.delete(data, node.left);
+    } else if (data > node.data) {
+      node.right = this.delete(data, node.right);
+    } else {
+      if (node.left === null) {
+        return node.right;
+      }
+
+      if (node.right === null) {
+        return node.left;
+      }
+    }
+
+    return node;
+  }
+
+  find(data, node = this.root) {
+    if (data === node.data) {
+      return node;
+    }
+
+    if (data < node.data) {
+      return this.find(data, node.left);
+    }
+
+    if (data > node.data) {
+      return this.find(data, node.right);
+    }
+
+    return node;
+  }
+
+  levelOrder(func = this.toArray) {
+    this.levelOrderTraversal = [];
+    const queue = [];
+
+    if (this.root === null) {
+      return;
+    }
+
+    queue.push(this.root);
+
+    while (queue.length > 0) {
+      const node = queue[0];
+      func(this.levelOrderTraversal, node.data);
+
+      if (node.left !== null) {
+         queue.push(node.left);
+      }
+      if (node.right !== null) {
+        queue.push(node.right);
+      }
+
+      queue.shift();
+    }
+    return this.levelOrderTraversal;
+  }
+
+  toArray(array, value) {
+    array.push(value);
+  }
+
+  inorder() {
+
+  }
+
+  preorder() {
+
+  }
+
+  postorder() {
+
+  }
+
+  height() {
+
+  }
+
+  depth() {
+
+  }
+
+  isBalanced() {
+
+  }
+
+  rebalance() {
+    
   }
 
   prettyPrint(node, prefix = '', isLeft = true) {
--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
 const tree = new Tree(array);
 
+tree.insert(2);
+tree.insert(32);
+
+tree.delete(32);
+tree.delete(2);
+
+
+tree.find(5);
+
+
 tree.prettyPrint(tree.root);
 
 
